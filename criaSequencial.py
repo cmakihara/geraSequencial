ca18e98c99b0dabc818a83f771050901809c249c
Claudio Makihara <claudio.makihara@ECO.LAN>
2023-12-22 07:58:58 -0300

    Ajustes

--- criaSequencial.py
+++ criaSequencial.py
@@ -1,5 +1,10 @@
 import os
 
+'''
+:gerarSequencial: Gera a sequencia de numeros entre as faixas informadas
+:param faixaNotas: Lista de faixa inicial e final
+:output: os numeros sequencias entra as faixa inicial e final
+''' 
 def gerarSequencial(faixaNotas):
     qtdNotas=0
     if(len(faixaNotas)%2 == 0):
@@ -7,21 +12,22 @@ def gerarSequencial(faixaNotas):
             ini = faixaNotas[i]
             fim = faixaNotas[i+1]
             if ini > fim:
-                print('\nALERTA: Contem faixa inicial menor que a final Verifique')
-                print(ini,fim)
-                input('\n\nPress ENTER to exit') 
+                print('ALERTA: Contem faixa inicial menor que a final Verifique\n')
+                print('Faixa inicial: ',ini,'\nFaixa final: ',fim,'\n')
+                input('Press ENTER to exit') 
                 exit()
 
             while(ini <= fim):
                 print(ini, end =",")
                 ini += 1
-                qtdNotas +=1;
-        print("\n\nTotal de numerações de notas geradas: ",qtdNotas)        
-        input('\n\nPress ENTER to exit')    
+                qtdNotas +=1
+        print("Total de numerações de notas geradas: ",qtdNotas,'\n')        
+        input('Press ENTER to exit')    
     else:
-        print("ERRO: QTD IMPAR das faixas de notas");
+        print("ERRO: QTD IMPAR das faixas de notas\n")
         inserirFaixas()
-        
+
+       
 def inserirFaixas():
     faixaInput = input('Insira as faixas de notas(numeros) separado por (,) Ex:12,15,55,55\n')
     faixaNotas = faixaInput.split(',') 
@@ -29,8 +35,8 @@ def inserirFaixas():
     for faixa in faixaNotas:
         if not(faixa.isnumeric()):
             print("ERRO somente (numero) e (,) permitido\n")
-            print('Caractere invalido: ',faixa)
-            input('\n\nPress ENTER to exit')   
+            print('Caractere invalido: ',faixa,'\n')
+            input('Press ENTER to exit')   
             exit()
     
     listNotas = list(map(int,faixaNotas))
